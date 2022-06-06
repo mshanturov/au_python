4eeeee3e75fdbee2e12eb4e59b831a32aa576c3e
mshanturov <92581780+mshanturov@users.noreply.github.com>
2022-06-06 20:37:16 +0300

    Update speed.py

--- Second/speed.py
+++ Second/speed.py
@@ -5,17 +5,33 @@ cur_time = time.time()
 
 
 @njit(fastmath=True)
-def simple_numbers(n):
+def simple_numbers_speed(n):
     for l in prange(2, n + 1):
         simple = True
         for i in range(2, l):
-            if l % i == 0:  
+            if l % i == 0:
                 simple = False
                 break
         if simple:
             number = l
 
 
-simple_numbers(120000)
-print(time.time()-cur_time)
 
+def simple_numbers(n):
+    for l in range(2, n + 1):
+        simple = True
+        for i in range(2, l):
+            if l % i == 0:
+                simple = False
+                break
+        if simple:
+            number = l
+
+
+simple_numbers_speed(120000)
+print('время работы с ускорением',time.time()-cur_time)
+
+
+cur_time = time.time()
+simple_numbers(120000)
+print('время рабрты без ускорения', time.time()-cur_time)
